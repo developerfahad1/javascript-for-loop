4dd0d2e84468584fd9936ec261ea823ea62f49bb
developerfahad1 <fahadgraphic.pk>
2024-03-26 14:34:23 +0500

    app.js and index.html file created

---
+++ app.js
@@ -0,0 +1,21 @@
+// Q1: write a program to display hello world five time for loops?
+
+// QUESTION ONE ANSWER
+
+// for (let i = 1; i <= 5; i++) {
+//    console.log('hello world');
+// }
+
+// Q2: write a program to print numeric counting from 1 to 10?
+
+// QUESTION TWO ANSWER
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// Q3: write a program to print multiplication table of any number using for loop table number & length should be taken and as an input from user 
+
+// QUESTION THREE ANSWER
+
+
